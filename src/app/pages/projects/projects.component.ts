8bd4999cf1ad277fa992fd8d886236975985a03b
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-07-24 20:00:37 -0400

    button animations

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BLOG, PROJECTS } from '../../app.constants';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
     NgOptimizedImage,
+    RouterLink,
   ],
   templateUrl: './projects.component.html',
 })
